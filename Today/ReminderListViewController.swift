28e437bdf5e56788dd18f41177e23a9b98ada0d4
Damian <bzdyrad@gmail.com>
2023-07-12 13:20:56 -0400

    create and apply a snapshot

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ReminderListViewController: UICollectionViewController {
     typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
     
     var dataSource: DataSource!
 
@@ -31,6 +32,18 @@ class ReminderListViewController: UICollectionViewController {
             return collectionView.dequeueConfiguredReusableCell(
                 using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
+        
+//        Create a new snapshot
+        var snapshot = Snapshot()
+//        Append sections to the snapshot (single section)
+        snapshot.appendSections([0])
+//        Append titles from sampleData to snapshot
+        snapshot.appendItems(Reminder.sampleData.map { $0.title })
+//        Apply the snapshot to the data source
+        dataSource.apply(snapshot)
+        
+//        Assign data source to the collection view
+        collectionView.dataSource = dataSource
     }
 
     private func listLayout() -> UICollectionViewCompositionalLayout {
